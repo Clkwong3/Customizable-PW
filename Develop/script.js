f28b6a1ca2c60edb3b052a47b963c29ff745882f
Clkwong3 <134983608+Clkwong3@users.noreply.github.com>
2023-07-04 18:16:04 -0700

    add function for prompts.

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,128 @@
 // Assignment code here
 // Default length is 8 characters
 var charLength = 8;
-var lowercaseArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var uppercaseArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'H', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-var numericArray = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
-var specialCharArray = [" ", "!", '"", "#", "$", "%", "&", "'", "(", ")", "*", "[", "]", "+", ",", "-", ".", "/", "\", ':', ';', '<', '>', '=', '?', '@', '^', '_','`', '{', '}', '|', '~'];
+var userChoice = [];
+
+var lowercaseArray = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+var uppercaseArray = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "H",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+
+var numericArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+
+var specialCharArray = [
+  " ",
+  "!",
+  "#",
+  "$",
+  "%",
+  "&",
+  "(",
+  ")",
+  "*",
+  "[",
+  "]",
+  "+",
+  ",",
+  "-",
+  ".",
+  "/",
+  ":",
+  ";",
+  "<",
+  ">",
+  "=",
+  "?",
+  "@",
+  "^",
+  "_",
+  "`",
+  "{",
+  "}",
+  "|",
+  "~",
+];
 
 // The red generate button
 var generateBtn = document.querySelector("#generate");
+
+// Prompts
+function promptInfo() {
+  charLength = parseInt(
+    prompt(
+      "How many characters would you like? Choose any number between 8 - 128"
+    )
+  );
+
+  if (confirm("Do you want to include lowercase letters?")) {
+    userChoice = userChoice(lowercaseArray);
+  }
+
+  if (confirm("Do you want to include uppercase letters?")) {
+    userChoice = userChoice(uppercaseArray);
+  }
+
+  if (confirm("Do you want to include numbers?")) {
+    userChoice = userChoice(numericArray);
+  }
+
+  if (confirm("Do you want to include special characters?")) {
+    userChoice = userChoice(specialCharArray);
+  }
+}
 
 // Define generatePassword
 function generatePassword() {
