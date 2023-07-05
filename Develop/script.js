9281177544624b854cf381dcdb77600d310fe100
Clkwong3 <134983608+Clkwong3@users.noreply.github.com>
2023-07-04 17:22:52 -0700

    filled in the arrays.

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,10 @@
 // Assignment code here
 // Default length is 8 characters
 var charLength = 8;
-var lowercaseArray = [];
-var uppercaseArray = [];
-var numericArray = [];
-var specialCharArray = [];
+var lowercaseArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var uppercaseArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'H', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var numericArray = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+var specialCharArray = [" ", "!", '"", "#", "$", "%", "&", "'", "(", ")", "*", "[", "]", "+", ",", "-", ".", "/", "\", ':', ';', '<', '>', '=', '?', '@', '^', '_','`', '{', '}', '|', '~'];
 
 // The red generate button
 var generateBtn = document.querySelector("#generate");
